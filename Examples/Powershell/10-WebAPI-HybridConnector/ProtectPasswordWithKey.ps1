2d650223d9c307b0eb40d2e216a8ed84607b69b1
Francois LEON <francois.leon@ubisoft.com>
2021-07-26 13:48:13 +0200

    Added script to generate encrypted password file

---
+++ Examples/Powershell/10-WebAPI-HybridConnector/ProtectPasswordWithKey.ps1
@@ -0,0 +1,10 @@
+﻿#Generate a random 256 bit AES key
+$Key = New-Object Byte[] 32
+[Security.Cryptography.RNGCryptoServiceProvider]::Create().GetBytes($Key)
+
+#Generate the encrypted password file with the previously created key and store it into a specific path.
+"your password" | ConvertTo-SecureString -AsPlainText -Force | ConvertFrom-SecureString -Key $key | Out-File 'C:\TEMP\secret.txt'
+
+# Create a PSCredential from the encrypted file with the key  
+# $OrgCreds = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList 'Corp SamAccountName', (Get-Content 'C:\TEMP\secret.txt' | ConvertTo-SecureString -Key $key)
+# Get-aduser "blabla" -credential $Orgcreds
